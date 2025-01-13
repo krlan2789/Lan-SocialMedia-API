ede08414083c6f4ed76804b8e123e6cf89d96ced
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2025-01-13 11:33:06 +0700

    Update member status route changes . .

--- LanGeng.API/Controllers/GroupController.cs
+++ LanGeng.API/Controllers/GroupController.cs
@@ -161,7 +161,7 @@ namespace LanGeng.API.Controllers
         }
 
         [Authorize]
-        [HttpPatch("g/{Slug}/{Status}")]
+        [HttpPatch("m/{Slug}/{Status}")]
         public async Task<IResult> UpdateMemberStatus(string Slug, GroupMemberStatusEnum Status)
         {
             try
